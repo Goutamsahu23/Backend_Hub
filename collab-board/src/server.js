6861464eb9db1b0c8e1e6819eb34e705dab07a90
Goutamsahu23 <goutamsahu602@gmail.com>
2025-08-17 21:14:01 +0530

    Project Configuration, DB and ws connection setup.

---
+++ collab-board/src/server.js
@@ -0,0 +1,42 @@
+const express=require('express');
+const dotenv=require('dotenv');
+const connectDB=require('./config/db');
+
+
+const { WebSocketServer } = require('ws');
+// const cors=require('cors');
+
+dotenv.config()
+connectDB();
+
+const app=express();
+app.use(express.json());
+// app.use(cors());
+
+// http server
+const PORT=process.env.PORT || 5000;
+const server=app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
+
+// WebSocket server
+const wss = new WebSocketServer({ server });
+
+wss.on('connection',(ws)=>{
+    console.log('New client connected');
+
+    ws.on('message',(message)=>{
+        console.log(`Received message: ${message.toString()}`);
+        // Broadcast the message to all connected clients
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocketServer.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on('close',()=>{
+        console.log('Client disconnected');
+    });
+})
+
